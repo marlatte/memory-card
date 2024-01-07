7f3741114b95794b4663f803c2321271e74dc6c2
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-07 17:03:14 -0500

    Move Cards into a table, centralize its CSS

---
+++ src/components/CardTable.jsx
@@ -0,0 +1,14 @@
+import Card from './Card';
+
+function CardTable() {
+  return (
+    <div className="card-table">
+      <Card name="Ted" />
+      <Card name="Beard" />
+      <Card name="Danny" />
+      <Card name="Roy" />
+    </div>
+  );
+}
+
+export default CardTable;
