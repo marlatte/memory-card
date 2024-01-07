2e388dd25cd1a0311df2b9df954f8d1cee7c4fb1
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-07 17:12:20 -0500

    Create cards dynamically

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,20 @@
 
 function Card({ name }) {
   return (
-    <div className="card">
+    <button
+      type="button"
+      className="card"
+      onClick={() => {
+        console.log(name);
+      }}
+    >
       <img
         src="https://picsum.photos/200/300"
         alt={name}
         className="player-pic"
       />
       <div className="player-name">{name}</div>
-    </div>
+    </button>
   );
 }
 
--- src/components/CardTable.jsx
+++ src/components/CardTable.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 import Card from './Card';
 
-function CardTable() {
+function CardTable({ names }) {
   return (
     <div className="card-table">
-      <Card name="Ted" />
-      <Card name="Beard" />
-      <Card name="Danny" />
-      <Card name="Roy" />
+      {names.map((name, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Card key={index} name={name} />
+      ))}
     </div>
   );
 }
