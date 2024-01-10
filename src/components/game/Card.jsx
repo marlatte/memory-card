e30d5f493871f0623c7a7ef59e3bf0c712155498
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-10 13:11:44 -0500

    Fix card size and name spacing

--- src/components/game/Card.jsx
+++ src/components/game/Card.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 
 function Card({ character, onClick }) {
+  const nameArray = character.name.split(' ');
+  const firstName = character.id === 892095 ? 'Dr. Sharon' : nameArray[0];
+  const lastName = nameArray[nameArray.length - 1];
+
   return (
     <button
       type="button"
@@ -9,7 +13,10 @@ function Card({ character, onClick }) {
       data-character-id={character.id}
     >
       <img src={character.img} alt={character.name} className="character-pic" />
-      <div className="character-name">{character.name}</div>
+      <div className="character-name">
+        <span className="first-name">{firstName}</span>
+        <span className="last-name">{lastName}</span>
+      </div>
     </button>
   );
 }
