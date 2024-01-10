2bdae98cc842742a2eee80e12aacba831ad918a7
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-10 12:16:01 -0500

    Style start modal, add welcome msg

--- src/components/modals/Start.jsx
+++ src/components/modals/Start.jsx
@@ -12,7 +12,13 @@ function StartModal({ setLevel, setStart }) {
   return (
     <section className="modal start">
       <div className="content">
-        <h1>Lasso&apos;s Starting Lineup</h1>
+        <h1>AFC Richmond needs your help!</h1>
+        <p>
+          Coach Lasso and the Greyhounds need to field teams for a charity
+          tournament raising money to{' '}
+          <span className="ital">teach Brett Goldstein how to run</span>. Pick
+          your players without doubling up and you&apos;re sure to win it all!
+        </p>
         <div className="buttons">
           {['easy', 'medium', 'hard'].map((word) => (
             <button
